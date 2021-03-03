51e2d9fba6c659425fb66bfe3096c32fddc59913
deansamcass <deansamcass@naver.com>
2021-03-03 17:59:28 +0900

    .

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -1,16 +1,33 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class login extends Activity {
+public class login extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        Button bt1 = (Button)findViewById(R.id.button2);
+
+        bt1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button2:
+                Intent intent1 = new Intent(this,home.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
 
